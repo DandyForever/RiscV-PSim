fff0e4a7558321e074b63c48b4ab7e7d64cc38fb
DandyForever <dandy4ever31@gmail.com>
2021-04-07 22:32:32 +0300

    Changed project folder

---
+++ src/elf_manager.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <gelf.h>
+#include <err.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+class ElfManager {
+public:
+    ElfManager() = delete;
+    ElfManager(const char* file_name);
+    ~ElfManager(){}
+
+    std::vector<uint8_t>& getWords() { return words; }
+    uint32_t getPC() { return PC; }
+
+private:
+    uint32_t PC;
+    std::vector<uint8_t> words;
+    std::vector<GElf_Phdr> phdrs;
+
+    void read_words(int fd);
+    void read_phdrs(Elf* elf, size_t phdr_num);
+};
